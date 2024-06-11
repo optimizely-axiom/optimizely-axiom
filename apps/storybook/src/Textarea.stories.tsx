66d1b40c55870be0cf099943545ab2547de7c5c9
Abu Jafar Saifullah <AbuJafar.Saifullah@optimizely.com>
2024-06-11 12:27:15 +0600

    add textarea story

---
+++ apps/storybook/src/Textarea.stories.tsx
@@ -0,0 +1,62 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Button, Text, Textarea } from "@optiaxiom/react";
+
+const meta: Meta<typeof Textarea> = {
+  component: Textarea,
+  parameters: {
+    layout: "centered",
+  },
+  title: "Components / Textarea",
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Textarea>;
+
+export const Basic: Story = {
+  args: {
+    isResizeable: false,
+    placeholder: "Write your text",
+  },
+};
+export const Highlight: Story = {
+  args: {
+    placeholder: "Write your text",
+  },
+};
+export const Custom: Story = {
+  args: {
+    bottomSection: <Button>Bottom</Button>,
+    placeholder: "Write your text",
+    topSection: <Text>Sample</Text>,
+  },
+};
+
+/* export const TypesOfTextarea: Story = {
+  render: () => (
+    <Flex flexDirection="column" gap="xl">
+      <Textarea
+        defaultValue="Disabled with value"
+        isDisabled={true}
+        placeholder="Disabled placeholder"
+      />
+
+      <Textarea isDisabled={true} placeholder="Disabled placeholder" />
+      <Textarea isInvalid={true} placeholder="Error state" />
+      <Textarea
+        defaultValue="Error with value"
+        isInvalid={true}
+        placeholder="Error with value"
+      />
+
+      <Textarea isDisabled={false} placeholder="Placeholder" />
+
+      <Textarea
+        defaultValue="This is a text Textarea"
+        placeholder="Placeholder"
+      />
+    </Flex>
+  ),
+};
+ */
