bb8744e6845260623914755cbfac2f502b54f907
toha993 <mutasim.toha@optimizely.com>
2024-06-24 21:05:32 +0600

    Rename radio to radio-group and update styling

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -12,7 +12,7 @@ export * from "./input";
 export * from "./kbd";
 export * from "./paper";
 export * from "./progress";
-export * from "./radio";
+export * from "./radioGroup";
 export * from "./separator";
 export * from "./skeleton";
 export * from "./sprinkles";
