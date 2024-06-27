af73bfaa07a89ab489a2d9e433ffa105120d7a3f
toha993 <mutasim.toha@optimizely.com>
2024-06-27 13:05:45 +0600

    Alert dialog

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,6 +1,7 @@
+export * from "./alert-dialog";
 export * from "./avatar";
-export * from "./box";
 
+export * from "./box";
 export * from "./button";
 export * from "./button-group";
 export * from "./code";
